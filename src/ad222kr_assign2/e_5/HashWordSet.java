2fe66fa7f857b0ed5c82a658bc5b48346fb1b169
ad222kr <ad222kr@student.lnu.se>
2016-09-20 17:40:55 +0200

    start wordset

---
+++ src/ad222kr_assign2/e_5/HashWordSet.java
@@ -0,0 +1,28 @@
+package ad222kr_assign2.e_5;
+
+import java.util.Iterator;
+
+/**
+ * Created by alex on 20.9.16.
+ */
+public class HashWordSet implements WordSet {
+  @Override
+  public void add(Word word) {
+
+  }
+
+  @Override
+  public boolean contains(Word word) {
+    return false;
+  }
+
+  @Override
+  public int size() {
+    return 0;
+  }
+
+  @Override
+  public Iterator iterator() {
+    return null;
+  }
+}
---
+++ src/ad222kr_assign2/e_5/WordSet.java
@@ -0,0 +1,8 @@
+package ad222kr_assign2.e_5;
+
+public interface WordSet extends Iterable {
+  void add(Word word); // Add word if not already added
+  boolean contains(Word word); // Return true if word contained
+  int size(); // Return current set size
+  String toString(); // Print contained words
+}

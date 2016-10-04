078315c4ff7938538aeda85412fc865e472e1593
ad222kr <ad222kr@student.lnu.se>
2016-10-04 16:18:50 +0200

    GML Done

--- directed_graph_[1].gml
+++ directed_graph_[1].gml
@@ -1,1 +1,94 @@
-fan
+graph [
+	node [
+		id 0
+		label "node no 0"
+		isHead "true"
+		isTail "false"
+	]
+	node [
+		id 1
+		label "node no 1"
+		isHead "false"
+		isTail "false"
+	]
+	node [
+		id 2
+		label "node no 2"
+		isHead "false"
+		isTail "false"
+	]
+	node [
+		id 3
+		label "node no 3"
+		isHead "false"
+		isTail "false"
+	]
+	node [
+		id 4
+		label "node no 4"
+		isHead "false"
+		isTail "false"
+	]
+	node [
+		id 5
+		label "node no 5"
+		isHead "false"
+		isTail "false"
+	]
+	node [
+		id 6
+		label "node no 6"
+		isHead "false"
+		isTail "true"
+	]
+	edge [
+		source 0
+		target 1
+		label "Edge from node 0 to node 1"
+	]
+	edge [
+		source 0
+		target 2
+		label "Edge from node 0 to node 2"
+	]
+	edge [
+		source 1
+		target 3
+		label "Edge from node 1 to node 3"
+	]
+	edge [
+		source 2
+		target 2
+		label "Edge from node 2 to node 2"
+	]
+	edge [
+		source 2
+		target 3
+		label "Edge from node 2 to node 3"
+	]
+	edge [
+		source 3
+		target 1
+		label "Edge from node 3 to node 1"
+	]
+	edge [
+		source 3
+		target 4
+		label "Edge from node 3 to node 4"
+	]
+	edge [
+		source 4
+		target 5
+		label "Edge from node 4 to node 5"
+	]
+	edge [
+		source 4
+		target 6
+		label "Edge from node 4 to node 6"
+	]
+	edge [
+		source 5
+		target 3
+		label "Edge from node 5 to node 3"
+	]
+]
--- src/ad222kr_assign3/MyGML.java
+++ src/ad222kr_assign3/MyGML.java
@@ -2,6 +2,13 @@ package ad222kr_assign3;
 
 import graphs.DirectedGraph;
 import graphs.GML;
+import graphs.Node;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+import static java.util.stream.Collectors.toMap;
 
 /**
  * Created by alex on 30.9.16.
@@ -14,6 +21,35 @@ public class MyGML<T> extends GML<T> {
 
   @Override
   public String toGML() {
-    return null;
+
+    StringBuilder sb = new StringBuilder();
+    sb.append("graph [\n");
+    for (T item : graph.allItems()) {
+      Node<T> node = graph.getNodeFor(item);
+      sb.append("\tnode [\n");
+      sb.append(String.format("\t\tid %d\n", item));
+      sb.append(String.format("\t\tlabel \"node no %d\"\n", item));
+      sb.append(String.format("\t\tisHead \"%s\"\n", node.isHead()));
+      sb.append(String.format("\t\tisTail \"%s\"\n", node.isTail()));
+      sb.append("\t]\n");
+    }
+
+    for (T item : graph.allItems()) {
+      Node<T> node = graph.getNodeFor(item);
+
+      Iterator<Node<T>> it = node.succsOf();
+        while (it.hasNext()) {
+          Node<T> next = it.next();
+          sb.append("\tedge [\n");
+          sb.append(String.format("\t\tsource %d\n", item));
+          sb.append(String.format("\t\ttarget %d\n", next.item()));
+          sb.append(String.format("\t\tlabel \"Edge from node %d to node %d\"\n", item, next.item()));
+          sb.append("\t]\n");
+        }
+
+    }
+
+    sb.append("]");
+    return sb.toString();
   }
 }

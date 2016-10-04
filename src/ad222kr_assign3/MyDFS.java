96cb8813e4a470fbd4a76270dc03a3a0166a4370
ad222kr <ad222kr@student.lnu.se>
2016-10-04 23:36:45 +0200

    DFS iterative n recursive done

--- src/ad222kr_assign3/MyDFS.java
+++ src/ad222kr_assign3/MyDFS.java
@@ -17,31 +17,14 @@ public class MyDFS<T> implements DFS<T> {
     if (root == null || graph == null)
       throw new NullPointerException();
 
+    boolean useRecursion = false;
 
-
-    List<Node<T>> toVisit = new ArrayList<>(); // To be visited next
-    List<Node<T>> visited = new ArrayList<>(); // Already visited
-    List<Node<T>> result = new ArrayList<>();
-
-
-    toVisit.add(root);
-    int num = 0; // WHY?
-    while (!toVisit.isEmpty()) {
-      Node<T> current = toVisit.remove(0);
-
-
-      if (!visited.contains(current)) {
-        current.num = num++;
-        visited.add(current);
-        result.add(current);
-        for (Iterator<Node<T>> it = current.succsOf(); it.hasNext();) {
-          Node<T> next = it.next();
-          toVisit.add(0, next);
-        }
-      }
+    if (useRecursion) {
+      ArrayList<Node<T>> visited = new ArrayList<>();
+      return dfs_recursive(graph, root, visited);
+    } else {
+      return dfs_iterative(graph, root);
     }
-
-    return result;
   }
 
   @Override
@@ -49,31 +32,17 @@ public class MyDFS<T> implements DFS<T> {
     if (graph == null)
       throw new NullPointerException();
 
-    List<Node<T>> toVisit = new ArrayList<>();
-    List<Node<T>> visited = new ArrayList<>();
-    List<Node<T>> result = new ArrayList<>();
+    boolean useRecursion = false;
 
-    graph.heads().forEachRemaining(toVisit::add);
-
-    int num = 0;
-    while (!toVisit.isEmpty()) {
-      Node<T> current = toVisit.remove(0);
-
-      if (!visited.contains(current)) {
-        current.num = num++;
-        visited.add(current);
-        result.add(current);
-        for (Iterator<Node<T>> it = current.succsOf(); it.hasNext();) {
-          Node<T> next = it.next();
-          toVisit.add(0, next);
-        }
-      }
+    if (useRecursion) {
+      ArrayList<Node<T>> visited = new ArrayList<>();
+      return dfs_recursive(graph, null, visited);
+    } else {
+      return dfs_iterative(graph, null);
     }
 
-    return result;
-
-
   }
+
 
 
   @Override
@@ -99,5 +68,71 @@ public class MyDFS<T> implements DFS<T> {
   @Override
   public List<Node<T>> topSort(DirectedGraph<T> graph) {
     return null;
+  }
+
+  /**
+   * An iterative approach to the depth first search algorithm.
+   * If the root parameter sent in is null, it starts searching
+   * from the head nodes of the graph.
+   *
+   * @param   graph the Graph to searh
+   * @param   root  element to start at
+   * @return        a List of all the nodes found
+   */
+  private List<Node<T>> dfs_iterative(DirectedGraph<T> graph, Node<T> root) {
+    List<Node<T>> toVisit = new ArrayList<>(); // To be visited next
+    List<Node<T>> visited = new ArrayList<>(); // Already visited
+
+    if (root == null) {
+      graph.heads().forEachRemaining(toVisit::add);
+    } else {
+      toVisit.add(root);
+    }
+
+    while (!toVisit.isEmpty()) {
+      Node<T> current = toVisit.remove(0);
+
+      if (!visited.contains(current)) {
+
+        visited.add(current);
+        current.num = visited.size();
+        for (Iterator<Node<T>> it = current.succsOf(); it.hasNext();) {
+          Node<T> next = it.next();
+          toVisit.add(0, next);
+        }
+      }
+    }
+    return visited;
+  }
+
+
+  /**
+   * A recursive approach to the depth first search algorithm
+   * If the root parameter sent in is null, it starts the search
+   * from the head nodes of the graph
+   *
+   * @param graph    the graph to search
+   * @param node     current node to check
+   * @param visited  array containing the visited elements, to keep track of em
+   * @return         a List containing all nodes when the recursion is done
+   */
+  private List<Node<T>> dfs_recursive(DirectedGraph<T> graph, Node<T> node, ArrayList<Node<T>> visited) {
+    // TODO: clean up dry
+    if (node == null) {
+      graph.heads().forEachRemaining(n -> {
+        if (!visited.contains(n)) {
+          visited.add(n);
+          n.num = visited.size();
+          n.succsOf().forEachRemaining(s -> dfs_recursive(graph, s, visited));
+        }
+      });
+    } else {
+      if (!visited.contains(node)) {
+        visited.add(node);
+        node.num = visited.size();
+        node.succsOf().forEachRemaining(s -> dfs_recursive(graph, s, visited));
+      }
+    }
+    return visited;
   }
 }
